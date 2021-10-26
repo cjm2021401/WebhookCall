65671618e6040840e5a33b8dcd42d280df90bdf4
mayf <cjm2021401@khu.ac.kr>
2021-10-26 13:15:16 +0900

    DOSC : First commit
    -

---
+++ src/main/java/whatap/io/ko/WebhookCall/WebhookCallApplication.java
@@ -0,0 +1,13 @@
+package whatap.io.ko.WebhookCall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/whatap/io/ko/WebhookCall/WebhookCallApplicationTests.java
@@ -0,0 +1,13 @@
+package whatap.io.ko.WebhookCall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

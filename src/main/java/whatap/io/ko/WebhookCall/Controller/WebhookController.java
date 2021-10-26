beda14b46e859b39e19315846296016a804f7c43
mayf <cjm2021401@khu.ac.kr>
2021-10-26 15:35:56 +0900

    Refactor : log작성 추가
    -

--- src/main/java/whatap/io/ko/WebhookCall/Controller/WebhookController.java
+++ src/main/java/whatap/io/ko/WebhookCall/Controller/WebhookController.java
@@ -1,5 +1,7 @@
 package whatap.io.ko.WebhookCall.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,9 @@ import whatap.io.ko.WebhookCall.Domain.Information;
 @RestController
 @RequestMapping("webhook")
 public class WebhookController {
+    private Logger logger = LoggerFactory.getLogger(WebhookController.class);
     @PostMapping(value = "/message")
     public void ReceiveWebhook(@RequestBody Information information){
-        System.out.println(information.getMessage());
+        logger.info("Received Webhook from "+information.getProjectName()+" : "+information.getMessage());
     }
 }
